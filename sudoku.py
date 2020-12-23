e691267f216afe3f30bb0b538f12ecc1659bfec4
Mohit <mohit.agarwalla2002@gmail.com>
2020-12-23 22:55:48 +0000

    GUI
    Initial GUI made. Code seems to work but there is some problem when GUI is initalised

--- sudoku.py
+++ sudoku.py
@@ -1,14 +1,16 @@
-board = [
-    [7,8,0,4,0,0,1,2,0],
-    [6,0,0,0,7,5,0,0,9],
-    [0,0,0,6,0,1,0,7,8],
-    [0,0,7,0,4,0,2,6,0],
-    [0,0,1,0,5,0,9,3,0],
-    [9,0,4,0,6,0,0,0,5],
-    [0,7,0,3,0,0,0,1,2],
-    [1,2,0,0,0,7,4,0,0],
-    [0,4,9,2,0,6,0,0,7]
-]
+# Inspired and aided greatly by Tech with Tim's code
+
+# board = [
+#     [7,8,0,4,0,0,1,2,0],
+#     [6,0,0,0,7,5,0,0,9],
+#     [0,0,0,6,0,1,0,7,8],
+#     [0,0,7,0,4,0,2,6,0],
+#     [0,0,1,0,5,0,9,3,0],
+#     [9,0,4,0,6,0,0,0,5],
+#     [0,7,0,3,0,0,0,1,2],
+#     [1,2,0,0,0,7,4,0,0],
+#     [0,4,9,2,0,6,0,0,7]
+# ]
 
 
 def solve(bo):
@@ -74,6 +76,6 @@ def find_empty(bo):
     return None # if no empty squares it will return None
 
 
-board_printing(board)
-solve(board)
-board_printing(board)
+# board_printing(board)
+# solve(board)
+# board_printing(board)
